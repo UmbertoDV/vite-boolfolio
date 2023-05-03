1af9af9a4876297fbf380554f7006173f524102b
UmbertoDV <biccoumby2002@hotmail.com>
2023-05-03 15:00:24 +0200

    added visual list in home api

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+
 export default {
 	data() {
 		return {
 			title: "Hello World",
+			cards: [],
 		};
 	},
+	components: {
+		AppHeader,
+	},
 
-	// components:{
-	//	 HelloWorld,
-	// }
+	methods: {
+		fetchCards() {
+			axios.get("http://127.0.0.1:8000/api/cards").then((response) => {
+				this.cards = response.data;
+			});
+		},
+	},
+
+	created() {
+		this.fetchCards();
+	},
 };
 </script>
 
 <template>
+	<AppHeader></AppHeader>
 	<h1>{{ title }}</h1>
+	<ul>
+		<li v-for="card in cards">{{ card.title }}</li>
+	</ul>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+	name: "AppHeader",
+	data() {
+		return {
+			title: "Hello World",
+		};
+	},
+};
+</script>
+
+<template>
+	<nav class="navbar bg-body-tertiary">
+		<div class="container-fluid">
+			<span class="navbar-brand mb-0 h1">Front Office</span>
+		</div>
+	</nav>
+</template>
+
+<style lang="scss" scoped>
+//SCSS
+</style>
